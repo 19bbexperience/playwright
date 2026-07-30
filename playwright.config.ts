1e0ce345367647e5277a1deed4ff20a72def60bd
R.Reynolds <99567501+19bbexperience@users.noreply.github.com>
2026-07-29 21:50:25 -0400

    Format 'use' object in playwright.config.ts
    Refactor Playwright configuration to format 'use' object.

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {actionTimeout: 15000}, 
+  use: {
+    actionTimeout: 15000, 
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://the-internet.herokuapp.com',
 
